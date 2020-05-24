8fb19cef53e54fc162d50f17119c772c7d9da7c3
David Swift <davidswift@Davids-MacBook-Pro.local>
2020-05-24 15:04:41 +0100

    Add client web app

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,21 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+  addr = 'New address';
+  balance = '1002937.90$';
+  transferTo= '';
+  amount= 0;
+  remarks= '';
+
+  transferEther(e){
+    alert('hello');
+    console.log(this.transferTo);
+  }
+}
