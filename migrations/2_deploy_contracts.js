a603851eb4b7a12414672b1aa4246939fb8c5eca
David Swift <davidswift@Davids-MacBook-Pro.local>
2020-05-25 10:09:52 +0100

    Add Payment contact to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const ApprovalContract = artifacts.require('ApprovalContract');
+const Payment = artifacts.require('Payment');
 
 module.exports = function(deployer) {
   deployer.deploy(ApprovalContract);
+  deployer.deploy(Payment);
 }
