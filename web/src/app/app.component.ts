b06e3a3a6904be36d48f0acdedcd4ec164f31969
David Swift <davidswift@Davids-MacBook-Pro.local>
2020-05-25 15:38:23 +0100

    Integrate Eth contract service with web app landing page

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,21 +1,50 @@
-
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+import {
+  EthcontractService
+} from './services/ethcontract.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
-  addr = 'New address';
-  balance = '1002937.90$';
-  transferTo= '';
-  amount= 0;
-  remarks= '';
 
-  transferEther(e){
-    alert('hello');
-    console.log(this.transferTo);
+export class AppComponent {
+  transferFrom = '0x0';
+  balance = '0 ETH';
+  transferTo = '';
+  amount = 0;
+  remarks = '';
+
+  constructor(private ethcontractService: EthcontractService) {
+    // use OnInit
+    this.initAndDisplayAccount();
+  }
+
+  initAndDisplayAccount = () => {
+    let that = this;
+    this.ethcontractService.getAccountInfo().then(function(acctInfo: any) {
+      that.transferFrom = acctInfo.fromAccount;
+      that.balance = acctInfo.balance;
+    }).catch(function(error) {
+      console.log(error);
+    });
+  };
+
+  transferEther(event) {
+    let that = this;
+    this.ethcontractService.transferEther(
+      this.transferFrom,
+      this.transferTo,
+      this.amount,
+      this.remarks
+    ).then(function() {
+      that.initAndDisplayAccount();
+    }).catch(function(error) {
+      console.log(error);
+      that.initAndDisplayAccount();
+    });
   }
 }
