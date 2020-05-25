48961d908b550b74d93b36392ec2bcf0e0ceeba8
David Swift <davidswift@Davids-MacBook-Pro.local>
2020-05-25 10:02:47 +0100

    Add payment contract

---
+++ contracts/Payment.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.16;
+
+contract Payment {
+  address transferFrom;
+  address transferTo;
+  uint paymentAmount;
+
+  constructor() public {
+    transferFrom = msg.sender;
+  }
+
+  event TransferFund(address _transferTo, address _transferFrom, uint amount);
+
+  function transferFund(address _transferTo) public payable returns (bool) {
+    transferTo = _transferTo;
+    transferTo.transfer(msg.value);
+    emit TransferFund(transferTo, transferFrom, msg.value);
+    return true;
+  }
+
+  function getBalanceOfCurrentAccount() public payable returns (uint) {
+    return transferFrom.balance;
+  }
+}
