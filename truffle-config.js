03f51228c3f8af1a41bb985403ec683b3389b179
David Swift <davidswift@Davids-MacBook-Pro.local>
2020-05-25 15:48:21 +0100

    Remove whitespace from config file

--- truffle-config.js
+++ truffle-config.js
@@ -48,10 +48,10 @@ module.exports = {
      network_id: "*",       // Any network (default: none)
     },
 
-    ganache : {
-      host : 'localhost',
-      port : 7545, // By default Ganache runs on this port.
-      network_id : '*' // network_id for ganache is 5777. However, by keeping * as value you can run this node on any network
+    ganache: {
+      host: 'localhost',
+      port: 7545, // By default Ganache runs on this port.
+      network_id: '*' // network_id for ganache is 5777. However, by keeping * as value you can run this node on any network
     },
 
     // Another network with more advanced options...
